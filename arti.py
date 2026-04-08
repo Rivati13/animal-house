b418d34bc76703eb6584ac1e15eb96805bd5dd27
Catherine Rivas <125322181+CatRivas@users.noreply.github.com>
2026-04-08 14:16:01 -0500

    art3miss page structure
    * add first line of the page
    
    * add slogan for the arti page
    
    * add description

---
+++ arti.py
@@ -0,0 +1,5 @@
+print("--------------- ART3MISE ART ------------------")
+
+print('Here will be the ARTI dedication page')
+
+print("THIS GUNTER IS THE FIRST TO FIND THE FIRST CHALLENGE FOR THE COOPER KEY")
